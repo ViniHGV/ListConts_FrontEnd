7c3bc8d05863ae0eda62e86640f054b5ff0fa054
Vini <ixxvinicius@gmail.com>
2023-03-12 19:24:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ListContacts from './Components/Contatos';
+
+function App() {
+  return( 
+  <div className='App'>
+    <ListContacts/>
+  </div>
+  );
+}
+
+export default App
