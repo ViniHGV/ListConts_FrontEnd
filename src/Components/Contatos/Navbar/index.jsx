40034c117abb65ac7167cac67de0705dadcd98a7
Vini <ixxvinicius@gmail.com>
2023-03-12 20:13:29 -0300

    Criando a navbar e estilizando

--- src/Components/Contatos/Navbar/index.jsx
+++ src/Components/Contatos/Navbar/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export function Navbar () {
-    return(
+export function Navbar() {
+    return (
         <div className="NavBar">
-            
+            <nav>
+                    <h1><a href="">ListCont</a></h1>
+                    <a href="">Pagina inicial</a>
+                    <a href="#">Contato</a>
+            </nav>
         </div>
     )
 }
