4404eaf21dddcd05b6196935d7a21c4f0ade696b
Vini <ixxvinicius@gmail.com>
2023-03-12 19:43:20 -0300

    Criando o component NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ListContacts from './Components/Contatos';
+import { Navbar } from './Components/Contatos/Navbar';
 
 function App() {
   return( 
   <div className='App'>
+    <Navbar/>
     <ListContacts/>
   </div>
   );
---
+++ src/Components/Contatos/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css'
+
+export function Navbar () {
+    return(
+        <div className="NavBar">
+            
+        </div>
+    )
+}
