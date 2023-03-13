d97211f0cedf108ceb61ad763a00aaac1d74a770
Vini <ixxvinicius@gmail.com>
2023-03-13 10:48:09 -0300

    Adicionando os boroes

--- src/Pages/Adicionar/index.jsx
+++ src/Pages/Adicionar/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Navbar } from "../../Components/Contatos/Navbar";
+import './index.css'
 
-export function Adicionar(){
+export function Adicionar() {
     return (
         <>
-        <Navbar/>
-        <h1>Adicionar</h1>
+            <Navbar />
+            <div className="containerLogin">
+                <div className="containerInputs">
+                    <h1>Adicionar Contato</h1>
+                    <form action="" className="formAdd">
+                        <input type="text" className="inputAdd" />
+                        <input type="text" className="inputAdd" />
+                        <input type="text" className="inputAdd" />
+                        <div className="buttonsForm">
+                        <button type="submit" className="btnForm">Adicionar</button>
+                        <button type="submit" className="btnForm">Limpar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
