45bf39a4c72d586267aa83330f3fedeeeddd0352
Vini <ixxvinicius@gmail.com>
2023-03-13 21:13:05 -0300

    Trabalhando com redimensionamento da renderização de contatos

--- src/Pages/Adicionar/index.jsx
+++ src/Pages/Adicionar/index.jsx
@@ -11,11 +11,11 @@ export function Adicionar() {
                     <h1>Adicionar Contato</h1>
                     <form action="" className="formAdd">
                         <label htmlFor="">Nome do Contato</label>
-                        <input type="text" className="inputAdd" />
+                        <input type="text" className="inputAdd"  placeholder="Digite o Nome do Contato:"/>
                         <label htmlFor=""className="lblInput">Url da imagem</label>
-                        <input type="text" className="inputAdd" />
+                        <input type="text" className="inputAdd" placeholder="Insira a Url da imagem do Contato:"/>
                         <label htmlFor="" className="lblInput">Telefone do Contato</label>
-                        <input type="text" className="inputAdd" />
+                        <input type="text" className="inputAdd"placeholder="Digite o Telefone do Contato:" />
                         <div className="buttonsForm">
                         <button type="submit" className="btnFormSub">Adicionar</button>
                         <button type="submit" className="btnFormCls">Limpar</button>
