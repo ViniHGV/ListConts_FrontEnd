b40ab9b5851844944f8eff66420a7abcdb13bbe2
Vini <ixxvinicius@gmail.com>
2023-03-14 22:33:45 -0300

    Aplicando estilos nos Links do Home e Inserindo o texto

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './index.css'
 import { Navbar } from "../../Components/Navbar";
+import { Link } from "react-router-dom";
 
 
 function HomePage() {
@@ -13,13 +14,25 @@ function HomePage() {
         <div className='pagContatosHome'>
           <div className='conteudoHome'>
             <h1> Objetivo do Projeto ListConts</h1>
-            <p>O projeto ListConts foi criado com o objetivo de aperfeiçoar o meu conhecimento com as técnologias:</p>
+            <p>O projeto ListConts foi criado com o objetivo de aperfeiçoar o meu conhecimento com as tecnologias:</p>
             <div className="React">
               <div className="ReactTitle">
                 <h2>React</h2>
                 <img src="https://img.shields.io/badge/-2A2E3B?style=for-the-badge&logo=react&logoColor=61DAFB"></img>
               </div>
-              <p> Uso de Hooks, Components, Routes, Use Form, Consumo de Api por meio do Axios </p>
+              <p> Uso de Hooks como: UseState, useEffect, Useform e validações de formularios com Yup e conceitos como Components, Routes e Consumo de Api por meio do Axios.</p>
+            </div>
+            <div className="React">
+              <div className="ReactTitle">
+                <h2>Asp Net web API</h2>
+                  <h6 className="logoNet">.NET</h6>
+              </div>
+              <p>API Criada com Migrations pela conexão com o banco de dados SQLSERVER com os métodos de CRUD para a criação de uma agenda de contatos.</p>
+            </div>
+            <div className="buttonsHome">
+              <Link to='/contatos'className="buttonsHmL">Lista de contatos</Link>
+              <Link to='adicionar'className="buttonsHmA">Adicionar contato</Link>
+              <Link to='excluir'className="buttonsHmC">Excluir Contato</Link>
             </div>
           </div>
         </div>
