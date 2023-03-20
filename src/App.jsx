16b6a615d7c5f7234dbe7449fa472be22650701e
Vini <ixxvinicius@gmail.com>
2023-03-20 20:49:48 -0300

    Organizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar } from './Components/Navbar';
 import ListContacts from './Pages/ListarContacts';
 import { Adicionar } from './Pages/Adicionar';
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import { Editar } from './Pages/Editar';
-import { Excluir } from './Pages/Excluir';
 import HomePage from './Pages/Home';
 
 
@@ -16,7 +14,6 @@ function App() {
           <Route path='/' element={<HomePage/>}/>
           <Route path='/contatos' element={<ListContacts/>}/>
           <Route path='/editar/:id' element={<Editar/>}/>
-          <Route path='/excluir' element={<Excluir/>}/>
           <Route path='/adicionar' element={<Adicionar/>}/>
         </Routes>
       </BrowserRouter>
